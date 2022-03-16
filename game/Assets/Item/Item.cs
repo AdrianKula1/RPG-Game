ad06f87aaa70ff61bb90a2059a864428d7134129
Chives51 <92929080+Chives51@users.noreply.github.com>
2022-03-16 13:50:12 +0100

    Add files via upload
    wstepna klasa Item

---
+++ game/Assets/Item/Item.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Item
+{
+    public enum itemType 
+    { 
+        weapon,
+        armor,
+        healthPotion,
+        manaPotion
+    }
+
+
+    public string name;
+    public string discription;
+    public int capacity;
+    public List<ItemStatistic> stat;
+    public itemType ItemType;
+    public int amount;
+
+    public string GetName()
+    {
+        return name;
+    }
+    public void SetName(string x) 
+    {
+        name = x;
+    }
+    public string GetDescroption()
+    {
+        return discription;
+    }
+    public void SetDescroption(string x)
+    {
+       discription = x;
+    }
+
+}
---
+++ game/Assets/Item/ItemStatistic.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemStatistic
+{
+    protected string name;
+    protected int value;
+
+    public Stat(string name)
+    {
+        this.name = name;
+        this.value = value;
+    }
+
+    public string GetStatName()
+    {
+        return name;
+    }
+    public int GetStatValue()
+    {
+        return value;
+    }
+}
